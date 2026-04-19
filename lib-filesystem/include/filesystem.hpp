559a4bd00cd0305dc220077ed7a5d5797f2b71b3
Connor Garey <connorgarey1999@gmail.com>
2026-04-19 22:46:50 +0100

    Added path checking functions

---
+++ lib-filesystem/include/filesystem.hpp
@@ -0,0 +1,15 @@
+#pragma once 
+
+#include <expected>
+
+#include <filesystem>
+#include <span>
+namespace FW::FS {
+  enum class ExistsErrors {
+    PATH_EMPTY,
+    COLLECTION_EMPTY,
+    PATH_DOES_NOT_EXIST,
+  };
+  [[nodiscard]] std::expected<bool, ExistsErrors> exists(const std::filesystem::path &path_to_item) noexcept;
+  [[nodiscard]] std::expected<void, ExistsErrors> validate_paths(const std::span<const std::filesystem::path> item_collection) noexcept;
+}
---
+++ lib-filesystem/src/filesystem.cpp
@@ -0,0 +1,28 @@
+#include "filesystem.hpp"
+
+#include <filesystem>
+#include <print>
+
+[[nodiscard]] std::expected<bool, FW::FS::ExistsErrors> FW::FS::exists(const std::filesystem::path &path_to_item) noexcept
+{
+  if (path_to_item.empty())
+    return std::unexpected(ExistsErrors::PATH_EMPTY);
+
+  return std::filesystem::exists(path_to_item);
+}
+
+[[nodiscard]] std::expected<void, FW::FS::ExistsErrors> validate_paths(const std::span<const std::filesystem::path> item_collection) noexcept
+{
+  if(item_collection.empty())
+    return std::unexpected(FW::FS::ExistsErrors::COLLECTION_EMPTY);
+
+  for(const auto& path : item_collection)
+  {
+    if(!std::filesystem::exists(path))
+    {
+      std::println(stderr, "Path [{}] does not exist!", path.string());
+      return std::unexpected(FW::FS::ExistsErrors::PATH_DOES_NOT_EXIST);
+    }
+  }
+  return {};
+}
