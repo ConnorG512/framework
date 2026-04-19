1e903742948a98309c27d938793ad2b4d9c75ce4
Connor Garey <connorgarey1999@gmail.com>
2026-04-19 23:25:37 +0100

    Get system path for windows and linux
    .

--- lib-filesystem/include/filesystem.hpp
+++ lib-filesystem/include/filesystem.hpp
@@ -15,4 +15,9 @@ enum class ExistsErrors
 [[nodiscard]] std::expected<bool, ExistsErrors> exists(const std::filesystem::path &path_to_item) noexcept;
 [[nodiscard]] std::expected<void, ExistsErrors>
 validate_paths(const std::span<const std::filesystem::path> item_collection) noexcept;
+
+enum class DataPathError {
+  NULL_ENV_VAR,
+};
+[[nodiscard]] std::expected<std::filesystem::path, DataPathError> get_system_path(const std::string &app_path_name = "fw") noexcept;
 } // namespace FW::FS
