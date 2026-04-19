1b43051a6231033d48ec3b5f991db6ba34fa8e8a
Connor Garey <connorgarey1999@gmail.com>
2026-04-19 23:01:15 +0100

    Formatting.

--- lib-filesystem/include/filesystem.hpp
+++ lib-filesystem/include/filesystem.hpp
@@ -1,15 +1,18 @@
-#pragma once 
+#pragma once
 
 #include <expected>
 
 #include <filesystem>
 #include <span>
-namespace FW::FS {
-  enum class ExistsErrors {
-    PATH_EMPTY,
-    COLLECTION_EMPTY,
-    PATH_DOES_NOT_EXIST,
-  };
-  [[nodiscard]] std::expected<bool, ExistsErrors> exists(const std::filesystem::path &path_to_item) noexcept;
-  [[nodiscard]] std::expected<void, ExistsErrors> validate_paths(const std::span<const std::filesystem::path> item_collection) noexcept;
-}
+namespace FW::FS
+{
+enum class ExistsErrors
+{
+  PATH_EMPTY,
+  COLLECTION_EMPTY,
+  PATH_DOES_NOT_EXIST,
+};
+[[nodiscard]] std::expected<bool, ExistsErrors> exists(const std::filesystem::path &path_to_item) noexcept;
+[[nodiscard]] std::expected<void, ExistsErrors>
+validate_paths(const std::span<const std::filesystem::path> item_collection) noexcept;
+} // namespace FW::FS
--- lib-filesystem/src/filesystem.cpp
+++ lib-filesystem/src/filesystem.cpp
@@ -3,7 +3,8 @@
 #include <filesystem>
 #include <print>
 
-[[nodiscard]] std::expected<bool, FW::FS::ExistsErrors> FW::FS::exists(const std::filesystem::path &path_to_item) noexcept
+[[nodiscard]] std::expected<bool, FW::FS::ExistsErrors>
+FW::FS::exists(const std::filesystem::path &path_to_item) noexcept
 {
   if (path_to_item.empty())
     return std::unexpected(ExistsErrors::PATH_EMPTY);
@@ -11,14 +12,15 @@
   return std::filesystem::exists(path_to_item);
 }
 
-[[nodiscard]] std::expected<void, FW::FS::ExistsErrors> validate_paths(const std::span<const std::filesystem::path> item_collection) noexcept
+[[nodiscard]] std::expected<void, FW::FS::ExistsErrors>
+validate_paths(const std::span<const std::filesystem::path> item_collection) noexcept
 {
-  if(item_collection.empty())
+  if (item_collection.empty())
     return std::unexpected(FW::FS::ExistsErrors::COLLECTION_EMPTY);
 
-  for(const auto& path : item_collection)
+  for (const auto &path : item_collection)
   {
-    if(!std::filesystem::exists(path))
+    if (!std::filesystem::exists(path))
     {
       std::println(stderr, "Path [{}] does not exist!", path.string());
       return std::unexpected(FW::FS::ExistsErrors::PATH_DOES_NOT_EXIST);
