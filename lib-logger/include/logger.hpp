5271dbcd38e19c4c0aeae4f7122cfd50fb17cf6e
Connor Garey <connorgarey1999@gmail.com>
2026-04-20 13:26:43 +0100

    Write to file

--- lib-logger/include/logger.hpp
+++ lib-logger/include/logger.hpp
@@ -1,15 +1,27 @@
-#pragma once 
+#pragma once
 
 #include <filesystem>
 #include <fstream>
 
-namespace FW::Log {
-  class Instance {
-    public:
-      explicit Instance(const std::filesystem::path &path);
-      Instance() = default;
-    
-    private:
-      std::ofstream file_ {"application.log", std::ios::trunc};
-  };
+namespace FW::Log
+{
+enum class Type
+{
+  INFO,
+  ERROR,
+  WARNING,
+  DEBUG,
 };
+
+class Instance
+{
+public:
+  explicit Instance(const std::filesystem::path &path);
+  Instance() = default;
+
+  void write(const FW::Log::Type type, const std::string &message);
+
+private:
+  std::ofstream file_{"application.log", std::ios::trunc};
+};
+}; // namespace FW::Log
--- lib-logger/src/logger.cpp
+++ lib-logger/src/logger.cpp
@@ -1,1 +1,31 @@
 #include "logger.hpp"
+#include <cassert>
+#include <print>
+
+namespace
+{
+[[nodiscard]] std::string_view get_logging_prefix(const FW::Log::Type prefix_type)
+{
+  switch (prefix_type)
+  {
+    case FW::Log::Type::ERROR:
+      return "ERROR";
+    case FW::Log::Type::WARNING:
+      return "WARNING";
+    case FW::Log::Type::DEBUG:
+      return "DEBUG";
+    case FW::Log::Type::INFO:
+      return "INFO";
+    default:
+      return "INVALID LOG USE";
+  }
+}
+} // namespace
+
+FW::Log::Instance::Instance(const std::filesystem::path &path) : file_{path, std::ios::trunc} {}
+
+void FW::Log::Instance::write(const Type type, const std::string &message)
+{
+  assert(!message.empty() && "Log message should not be empty!");
+  std::println(file_, "[{}]: {}.", get_logging_prefix(type), message);
+}
