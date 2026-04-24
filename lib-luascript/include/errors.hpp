befdaf2f8ec03844c2c2aab58e690561e1f075fa
Connor Garey <connorgarey1999@gmail.com>
2026-04-24 12:33:01 +0100

    Added more error sets

--- lib-luascript/include/errors.hpp
+++ lib-luascript/include/errors.hpp
@@ -5,6 +5,7 @@ namespace FW::LS
 enum class LuaCPushErr
 {
   FUNC_IS_NULLPTR,
+  SIGNATURE_IS_NULLPTR,
   LIST_EMPTY,
 };
 
@@ -14,5 +15,16 @@ enum class LuaPullValErr
   NO_VALID_RETURN_VALUE,
   RECIEVED_STRING_IS_NULL,
   GLOBAL_DOES_NOT_EXIST,
+};
+
+enum class ToTypeErr {
+  EMPTY_KEY,
+  CANNOT_FIND_VALID_TYPE,
+};
+
+enum class ExecFileErr{
+  EMPTY_ROOT_PATH,
+  EMPTY_FILE_LIST,
+  FAILED_TO_EXEC_LUA_FILE,
 };
 } // namespace FW::LS
